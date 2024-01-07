6f798e293e27d7852440a806b199a6c098ada769
bek77g <d.zheenkul@gmail.com>
2024-01-08 02:23:54 +0600

    add: cookie save token

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,14 +1,25 @@
 'use client';
 import useAuth from '@/hooks/useAuth';
 import Cookies from 'js-cookie';
+import { usePathname, useRouter } from 'next/dist/client/components/navigation';
 import { PropsWithChildren, useEffect } from 'react';
 
 export function AuthProvider({ children }: PropsWithChildren) {
 	const { user, isLoggedIn } = useAuth();
+	const pathname = usePathname();
+	const router = useRouter();
+
 	useEffect(() => {
 		if (isLoggedIn) {
 			Cookies.set('token', user?.jwt ?? '');
 		}
 	}, [user, isLoggedIn]);
+
+	useEffect(() => {
+		if (pathname !== '/login' && pathname !== '/register') {
+			if (!isLoggedIn) router.push('/login');
+		}
+	}, [pathname]);
+
 	return children;
 }
