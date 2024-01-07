feb89d0925ae4abfdcd961499f5ab5a4dee318ff
bek77g <d.zheenkul@gmail.com>
2024-01-07 19:49:24 +0600

    add: active state of links

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -1,17 +1,26 @@
-import siderbarLinks from '@/components/layout/Sidebar/siderbarLinks';
+'use client';
+import { MENU } from '@/components/layout/Sidebar/sidebar.data';
+import cn from 'clsx';
 import { Sun } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './sidebar.module.scss';
 
 export function Sidebar() {
+	const pathname = usePathname();
+
 	return (
 		<aside className={styles.sidebar}>
 			<Image src='/logo.svg' priority alt='ICO' width={40} height={40} />
 			<ul>
-				{siderbarLinks.map(({ icon: Icon, link }, idx) => (
-					<li key={idx}>
-						<Link href={link}>
+				{MENU.map(({ icon: Icon, url }) => (
+					<li
+						key={url}
+						className={cn({
+							[styles.active]: pathname === url,
+						})}>
+						<Link href={url}>
 							<Icon size={27} />
 						</Link>
 					</li>
