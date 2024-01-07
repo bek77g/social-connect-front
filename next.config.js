6e4eed332160e8409db2b514bf4f272845ff779c
bek77g <d.zheenkul@gmail.com>
2024-01-08 01:06:05 +0600

    fix: nextauth envs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	API_URL: process.env.API_URL,
+	NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 };
 
 module.exports = nextConfig;
