1f111b5b0773d1be9057633623b626912ef4e5f4
bek77g <d.zheenkul@gmail.com>
2024-01-07 19:49:36 +0600

    refactor: names of sidebar data

---
+++ src/components/layout/Sidebar/sidebar.data.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@/configs/links.config';
+import { MessagesSquare, Phone, Settings, Users2 } from 'lucide-react';
+
+export const MENU = [
+	{
+		icon: Users2,
+		url: Routes.FRIENDS,
+	},
+	{
+		icon: Phone,
+		url: Routes.CALL,
+	},
+	{
+		icon: MessagesSquare,
+		url: Routes.CHATS,
+	},
+	{
+		icon: Settings,
+		url: Routes.SETTINGS,
+	},
+];
