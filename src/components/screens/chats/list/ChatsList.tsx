ff909c80bd9e960565a8b4d5f2a8b38644d7861a
bek77g <d.zheenkul@gmail.com>
2024-01-09 02:20:22 +0600

    add: react-query lib

--- src/components/screens/chats/list/ChatsList.tsx
+++ src/components/screens/chats/list/ChatsList.tsx
@@ -1,12 +1,33 @@
+'use client';
+import { $fetch } from '@/$api/api.fetch';
+import { ChatListItem } from '@/components/screens/chats/list/ChatListItem';
 import Field from '@/components/ui/Field';
+import { Loader } from '@/components/ui/Loader';
+import { IChat } from '@/types/chat.types';
+import { useQuery } from '@tanstack/react-query';
 import { Search } from 'lucide-react';
 
 export function ChatsList() {
+	const { data, isLoading } = useQuery({
+		queryKey: ['chats'],
+		queryFn: () => $fetch.get<IChat[]>('/cahts', {}, true),
+	});
+
 	return (
 		<>
 			<div className='border-t border-b border-border p-layout'>
 				<Field placeholder='Search chat' Icon={Search} />
-				<ul></ul>
+			</div>
+			<div>
+				{isLoading ? (
+					<div className='p-layout'>
+						<Loader />
+					</div>
+				) : data?.length ? (
+					data.map(chat => <ChatListItem key={chat.id} {...chat} />)
+				) : (
+					<p className='p-layout'>Chats not found!</p>
+				)}
 			</div>
 		</>
 	);
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,13 +1,18 @@
 'use client';
 
 import { AuthProvider } from '@/providers/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: PropsWithChildren) {
 	return (
 		<SessionProvider>
-			<AuthProvider>{children}</AuthProvider>
+			<QueryClientProvider client={queryClient}>
+				<AuthProvider>{children}</AuthProvider>
+			</QueryClientProvider>
 		</SessionProvider>
 	);
 }
