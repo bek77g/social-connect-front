e11b8b3027f380e9d868c05aab13a47755ef17a8
bek77g <d.zheenkul@gmail.com>
2024-01-09 01:37:49 +0600

    add: current user base template

--- src/components/screens/chats/CurrentsUser.tsx
+++ src/components/screens/chats/CurrentsUser.tsx
@@ -1,3 +1,22 @@
+'use client';
+import useAuth from '@/hooks/useAuth';
+import Image from 'next/image';
+
 export function CurrentsUser() {
-	return <div className='p-layout'>CurrentsUser</div>;
+	const { user } = useAuth();
+
+	return (
+		<div className='p-layout flex items-center gap-4'>
+			<Image
+				width={50}
+				height={50}
+				src={user?.avatar ?? '/no-avatar.png'}
+				alt={user?.email ?? 'USER'}
+			/>
+			<div className='text-sm'>
+				<p>{user?.username}</p>
+				<p className='opacity-30'>General user</p>
+			</div>
+		</div>
+	);
 }
