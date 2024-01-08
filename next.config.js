cb664925547479b8dcea536ef7634e0fc3b122bd
bek77g <d.zheenkul@gmail.com>
2024-01-09 03:56:27 +0600

    add: new config file getImageUrl

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	NEXT_PUBLIC_API_URL: process.env.API_URL,
+	NEXT_PUBLIC_BACK_URL: process.env.NEXT_PUBLIC_BACK_URL,
+	NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
 	NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+	images: {
+		domains: ['127.0.0.1'],
+	},
 };
 
 module.exports = nextConfig;
