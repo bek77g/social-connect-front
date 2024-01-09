f67545f05e3f38634e700af07724a839c0ddba36
bek77g <d.zheenkul@gmail.com>
2024-01-09 23:28:14 +0600

    add: set only users filter fetch

--- src/components/screens/chats/list/ChatsList.tsx
+++ src/components/screens/chats/list/ChatsList.tsx
@@ -4,16 +4,24 @@ import { ChatsListItem } from '@/components/screens/chats/list/ChatsListItem';
 import Field from '@/components/ui/Field';
 import { Loader } from '@/components/ui/Loader';
 import useAuth from '@/hooks/useAuth';
+import { useDebounce } from '@/hooks/useDebounce';
 import { useQuery } from '@tanstack/react-query';
 import { Search } from 'lucide-react';
+import { useState } from 'react';
 
 export function ChatsList() {
 	const { isLoggedIn } = useAuth();
+	const [searchTerm, setSearchTerm] = useState('');
+	const debouncedSearchTerm = useDebounce(searchTerm, 300);
+
 	const { data, isLoading } = useQuery({
-		queryKey: ['chats'],
+		queryKey: ['chats', debouncedSearchTerm],
 		queryFn: () =>
 			$fetch.get<{ data: IChat[] }>(
-				'/chats?populate[messages]=*&populate[participants][populate][avatar]=*',
+				`/chats?sort=createdAt:desc
+				&populate[messages]=*
+				&populate[participants][populate][avatar]=*
+				&filters[participants][username][$containsi]=${debouncedSearchTerm}`,
 				{},
 				true
 			),
@@ -23,7 +31,12 @@ export function ChatsList() {
 	return (
 		<>
 			<div className='border-t border-b border-border p-layout'>
-				<Field placeholder='Search chat' Icon={Search} />
+				<Field
+					placeholder='Search chat'
+					Icon={Search}
+					value={searchTerm}
+					onChange={({ target }) => setSearchTerm(target.value)}
+				/>
 			</div>
 			<div>
 				{isLoading ? (
