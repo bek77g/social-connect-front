fdee124fc7b6e9eff868a21be0a72989c1921997
bek77g <d.zheenkul@gmail.com>
2024-01-11 03:43:08 +0600

    add: active chat effect

--- src/components/screens/chats/list/ChatsListItem.tsx
+++ src/components/screens/chats/list/ChatsListItem.tsx
@@ -1,18 +1,26 @@
 import { Avatar } from '@/components/ui/Avatar';
 import useAuth from '@/hooks/useAuth';
 import { IChat } from '@/types/chat.types';
+import cn from 'clsx';
 import dayjs from 'dayjs';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
 export function ChatsListItem({ participants, messages, id }: IChat) {
 	const { user } = useAuth();
+	const { id: activeChatId } = useParams<{ id: string }>();
 	const correspondent = participants.find(u => u.email !== user?.email);
 	const lastMessage = messages.at(-1);
 
 	return (
 		<Link
 			href={`/chat/${id}`}
-			className='p-layout flex items-center gap-4 border-b border-border duration-300 ease-linear transition-colors hover:bg-border animation-slide-fade'>
+			className={cn(
+				'p-layout flex items-center gap-4 border-b border-border duration-300 ease-linear transition-colors hover:bg-border animation-slide-fade',
+				{
+					'bg-border': id === +activeChatId,
+				}
+			)}>
 			<Avatar user={correspondent} width={45} height={45} />
 			<div className='text-sm w-full'>
 				<div className='flex items-center justify-between'>
