40dbcc7781b79000371e7a78511d506e66f2e85e
bek77g <d.zheenkul@gmail.com>
2024-01-11 04:24:25 +0600

    add: useProfile hook

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,15 @@
+import { $fetch } from '@/$api/api.fetch';
+import { IUser } from '@/types/user.types';
+import { useQuery } from '@tanstack/react-query';
+
+export function useProfile() {
+	return useQuery({
+		queryKey: ['profile'],
+		queryFn: () =>
+			$fetch.get<IUser>(
+				'/users/me?populate[avatar]=*&populate[friends][avatar]=*',
+				{},
+				true
+			),
+	});
+}
