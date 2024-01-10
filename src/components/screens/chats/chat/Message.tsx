2f4d9a8f3573fb5bf21a803c6ec632e629c1d399
bek77g <d.zheenkul@gmail.com>
2024-01-11 00:39:34 +0600

    fix: message is sender output

--- src/components/screens/chats/chat/Message.tsx
+++ src/components/screens/chats/chat/Message.tsx
@@ -6,38 +6,38 @@ import dayjs from 'dayjs';
 
 export function Message({ message }: { message: IMessage }) {
 	const { user } = useAuth();
-	const isSender = user.email === message.sender.email;
+	const isSender = user?.email === message.sender.email;
 
 	return (
 		<div
 			className={cn('flex mb-2.5', {
-				'justify-start': isSender,
-				'justify-end': !isSender,
+				'justify-end': isSender,
+				'justify-start': !isSender,
 			})}>
 			<div
 				className={cn('relative flex items-start', {
-					'flex-row-reverse': !isSender,
+					'flex-row-reverse': isSender,
 				})}>
 				<Avatar user={message.sender} className='rounded-full' />
 				<div
 					className={cn('', {
-						'ml-3': isSender,
-						'mr-3': !isSender,
+						'mr-3': isSender,
+						'ml-3': !isSender,
 					})}>
 					<div
 						className={cn(
 							'text-sm text-white bg-gray-200 mt-1 py-1.5 px-3 rounded-lg rounded-2xl',
 							{
-								'rounded-tl-none bg-border': isSender,
-								'rounded-tr-none bg-primary': !isSender,
+								'rounded-tr-none bg-primary': isSender,
+								'rounded-tl-none bg-border': !isSender,
 							}
 						)}>
 						<p>{message.text}</p>
 					</div>
 					<span
 						className={cn('text-xs text-gray-500 block mt-2', {
-							'text-left': isSender,
-							'text-right': !isSender,
+							'text-right': isSender,
+							'text-left': !isSender,
 						})}>
 						{dayjs(message.createdAt).format('HH:mm')}
 					</span>
