bef0825501a48d467dafee285d7d8b3376681372
bek77g <d.zheenkul@gmail.com>
2024-01-10 19:28:57 +0600

    fix: content render condition

--- src/components/screens/chats/ChatsScreen.tsx
+++ src/components/screens/chats/ChatsScreen.tsx
@@ -5,15 +5,19 @@ import { PropsWithChildren } from 'react';
 interface IChatsScreen extends PropsWithChildren {}
 
 export function ChatsScreen({ children }: IChatsScreen) {
+	const Content = () =>
+		children ? (
+			<>{children}</>
+		) : (
+			<p className='p-layout'>Click chat on the left side</p>
+		);
 	return (
 		<div className='grid grid-cols-[0.7fr_3fr] h-full'>
 			<div className='border-r border-border'>
 				<CurrentsUser />
 				<ChatsList />
 			</div>
-			{<>{children}</> && (
-				<p className='p-layout'>Click chat on the left side</p>
-			)}
+			{<>{children}</> && <Content />}
 		</div>
 	);
 }
